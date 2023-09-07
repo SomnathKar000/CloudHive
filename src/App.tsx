98f8a31e7a0a9f3384b952ab6e6e258e0da52bd1
SomnathKar000 <somnathkar201901@gmail.com>
2023-09-07 17:45:13 +0530

    added tailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>CloudHive</h1>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
